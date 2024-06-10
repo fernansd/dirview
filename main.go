a3bd2fd11522851afd8422a9d6e1ac36a203fb98
Fernando S. <code@fernansd.dev>
2024-06-11 00:04:53 +0200

    Get directory entries

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+_    "io/fs"
+    "log"
+    "os"
+    "path/filepath"
+)
+
+func readDir(path string) ([]string, error) {
+    entries, err := os.ReadDir(path)
+    if err != nil {
+        return nil, err
+    }
+
+    var filenames []string
+    for _, entry := range entries {
+        filenames = append(filenames, entry.Name())
+    }
+
+    return filenames, nil
+}
 
 func main() {
-    fmt.Println("Hello from dirview!")
+    targetDir := "."
+    absolutePath, _ := filepath.Abs(targetDir)
+    fmt.Printf("Listing contents for current directory: %s\n", absolutePath)
+    files, err := readDir(targetDir)
+    if err != nil  {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        fmt.Println(file)
+    }
+        
+
 }
+
+
+
